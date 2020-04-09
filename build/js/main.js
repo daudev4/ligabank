e27ee0f0e3eed077aed6c0ed264118eb0105b798
daudev4 <daud_08@mail.ru>
2020-04-09 10:54:39 +0300

    Исправляет баги, добавляет реализацию мобильного меню

--- build/js/main.js
+++ build/js/main.js
@@ -1126,6 +1126,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
 'use strict';
 
+(function () {
+  var WIDTH_TABLET = 768;
+
+  document.body.classList.remove('no-js');
+
+  var menu = function () {
+    var root = document.querySelector('.menu');
+    var menuButton = root.querySelector('.menu__toggle');
+    var menuItems = root.querySelectorAll('.menu__item');
+
+    var onMenuItemClick = function () {
+      root.classList.toggle('menu_closed');
+      document.body.classList.toggle('no-scroll');
+    };
+
+    menuButton.addEventListener('click', function () {
+      root.classList.toggle('menu_closed');
+      document.body.classList.toggle('no-scroll');
+    });
+
+    window.addEventListener('resize', function () {
+      if (window.innerWidth >= WIDTH_TABLET) {
+        menuItems.forEach(function (item) {
+          item.removeEventListener('click', onMenuItemClick);
+        });
+      } else {
+        menuItems.forEach(function (item) {
+          item.addEventListener('click', onMenuItemClick);
+        });
+      }
+    });
+  };
+
+  menu();
+})();
+
+'use strict';
+
 var colorSelect = new CustomSelect({
   elem: 'calculator-purpose'
 });
